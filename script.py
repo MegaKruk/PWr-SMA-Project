6a690501874e1dfb47413c2343e1f958df3f9150
Felisia Ryukichi <felisiaryukiya@gmail.com>
2019-11-24 13:05:29 +0100

    Split data to training set and test set
    Data is not cleaned yet. 
    This script is used to take 0% of data as training set and 20% as test set.
    Library used is sklear.

--- script.py
+++ script.py
@@ -1,4 +1,17 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 df = pd.read_excel (r'/home/megakruk/workspace/PycharmProjects/PWr-SMA-Project/online_retail_II.xlsx')
 print(df)
+x=df.loc[:, ["InvoiceDate"]] 
+y=df.loc[:,["Invoice","StockCode","Description","Quantity","Price","Country"]] 
+x_train, x_test, y_train, y_test =train_test_split(x,y, test_size=1/5, random_state=0)
+
+print (len(x_train))
+print (len(y_train))
+
+print (len(x_test))
+print (len(y_test))
